5c5fa2d53962e00b556141c02780ef0ea93b447a
Ralph Largo <ralph.largo@volenday.com>
2017-08-07 20:20:29 +0800

    fix login

--- routes/api.js
+++ routes/api.js
@@ -3,36 +3,39 @@ var router = express.Router();
 var api = require('../models/models');
 var bcrypt = require('bcrypt');
 
-router.post('/authenticate', function(req, res, next){
-  api.User.where({ email: req.body.email }).fetch().then(function(response){
-    console.log(response.password)
-    // if(response == null){
-    //   res.send({ message: 'There is no such user.'})
-    // }else{
-    //   console.log(response.password)
-    //   bcrypt.compare(req.body.password, response.password, function(err, res){
-    //     console.log(res)
-    //     console.log(err)
-    //   })
-    // }
-  }).catch(function(err){
+router.post('/authenticate', function (req, res, next) {
+  api.User.where({ email: req.body.email }).fetch().then(function (response) {
+    if (!response) {
+      res.send({ message: 'There is no such user.' })
+    } else {
+      response = response.toJSON();
+      bcrypt.compare(req.body.password, response.password, function (err, user) {
+        if (user) {
+          delete response.password;
+          res.send(response);
+        } else {
+          res.send({ message: 'Invalid username and password. '});
+        }
+      })
+    }
+  }).catch(function (err) {
     res.send({ error: err })
   })
 })
 
-router.get('/user', function(req, res, next) {
-  api.User.fetchAll({withRelated: ['info']}).then(function(response){
+router.get('/user', function (req, res, next) {
+  api.User.fetchAll({ withRelated: ['info'] }).then(function (response) {
     res.send({ status: true, response: response });
-  }).catch(function(error){
-    res.send({ status: false, error});
+  }).catch(function (error) {
+    res.send({ status: false, error });
   });
 });
 
-router.get('/user/:id', function(req, res, next){
-  api.UserInfo.where({ id: req.params.id }).fetch({withRelated: ['user', 'job_title']}).then(function(response){
+router.get('/user/:id', function (req, res, next) {
+  api.UserInfo.where({ id: req.params.id }).fetch({ withRelated: ['user', 'job_title'] }).then(function (response) {
     res.send({ status: true, response });
-  }).catch(function(error){
-    res.send({ status: false, error: error});
+  }).catch(function (error) {
+    res.send({ status: false, error: error });
   });
 });
 
