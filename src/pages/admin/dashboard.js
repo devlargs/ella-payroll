563933858fb837f2b3a969bff1d6d0441a915a9c
Ralph Largo <devlargs@gmail.com>
2017-08-23 01:50:54 +0800

    test datepicker

--- src/pages/admin/dashboard.js
+++ src/pages/admin/dashboard.js
@@ -26,6 +26,10 @@ class App extends React.Component {
         var a = getDates(new Date('2017-08-01'), new Date('2017-08-15'))
         console.log(a)
 
+        $('.input-daterange').datepicker({
+            todayHighlight: true
+        })
+
     }
 
     render() {
@@ -35,13 +39,34 @@ class App extends React.Component {
             <div>
                 <section className="content-header">
                     <h1>
-                        Shantidope
-                    <small>it all starts here</small>
+                        Dashboard
                     </h1>
                 </section>
 
                 <section className="content">
+                    <div class="box box-info">
+                    <div class="box-header with-border">
+                    <h3 class="box-title">DTR Update</h3>
 
+                    <div class="box-tools pull-right">
+                        <button type="button" class="btn btn-box-tool" data-widget="collapse"><i class="fa fa-minus"></i>
+                        </button>
+                    </div>
+                    </div>
+                    
+                    <div class="box-body">  
+                        <div class="input-group input-daterange">
+                            <input type="text" class="form-control" value="2012-04-05"/>
+                            <div class="input-group-addon">to</div>
+                            <input type="text" class="form-control" value="2012-04-19"/>
+                        </div>
+                    </div>
+
+                    <div class="box-footer clearfix">
+                    <a href="javascript:void(0)" class="btn btn-sm btn-info btn-flat pull-left">Place New Order</a>
+                    <a href="javascript:void(0)" class="btn btn-sm btn-default btn-flat pull-right">View All Orders</a>
+                    </div>
+                </div>
                 </section>
             </div>
         )
